76b5fa25e4b621df3250e4ac60537f80fbcb1c2d
egesenkul <egesenkul35@hotmail.com>
2018-03-23 22:04:23 +0300

    Proje yayımlanmıştır

---
+++ 2OyunculuZarOyunu/2OyunculuZarOyunu/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace _2OyunculuZarOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.skorOyuncu1 = new System.Windows.Forms.Label();
+            this.skorOyuncu2 = new System.Windows.Forms.Label();
+            this.btnOyuncu1 = new MetroFramework.Controls.MetroButton();
+            this.btnOyuncu2 = new MetroFramework.Controls.MetroButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureBox1.Image = global::_2OyunculuZarOyunu.Properties.Resources.zar6;
+            this.pictureBox1.Location = new System.Drawing.Point(309, 105);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(112, 112);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // skorOyuncu1
+            // 
+            this.skorOyuncu1.AutoSize = true;
+            this.skorOyuncu1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.skorOyuncu1.Location = new System.Drawing.Point(98, 243);
+            this.skorOyuncu1.Name = "skorOyuncu1";
+            this.skorOyuncu1.Size = new System.Drawing.Size(23, 25);
+            this.skorOyuncu1.TabIndex = 3;
+            this.skorOyuncu1.Text = "0";
+            this.skorOyuncu1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // skorOyuncu2
+            // 
+            this.skorOyuncu2.AutoSize = true;
+            this.skorOyuncu2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.skorOyuncu2.Location = new System.Drawing.Point(585, 243);
+            this.skorOyuncu2.Name = "skorOyuncu2";
+            this.skorOyuncu2.Size = new System.Drawing.Size(23, 25);
+            this.skorOyuncu2.TabIndex = 4;
+            this.skorOyuncu2.Text = "0";
+            this.skorOyuncu2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnOyuncu1
+            // 
+            this.btnOyuncu1.BackColor = System.Drawing.Color.Transparent;
+            this.btnOyuncu1.Location = new System.Drawing.Point(39, 114);
+            this.btnOyuncu1.Name = "btnOyuncu1";
+            this.btnOyuncu1.Size = new System.Drawing.Size(125, 91);
+            this.btnOyuncu1.TabIndex = 24;
+            this.btnOyuncu1.Text = "ZAR AT";
+            this.btnOyuncu1.UseSelectable = true;
+            this.btnOyuncu1.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // btnOyuncu2
+            // 
+            this.btnOyuncu2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOyuncu2.BackColor = System.Drawing.Color.Transparent;
+            this.btnOyuncu2.Location = new System.Drawing.Point(550, 114);
+            this.btnOyuncu2.Name = "btnOyuncu2";
+            this.btnOyuncu2.Size = new System.Drawing.Size(125, 91);
+            this.btnOyuncu2.TabIndex = 25;
+            this.btnOyuncu2.Text = "ZAR AT";
+            this.btnOyuncu2.UseSelectable = true;
+            this.btnOyuncu2.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 293);
+            this.Controls.Add(this.btnOyuncu2);
+            this.Controls.Add(this.btnOyuncu1);
+            this.Controls.Add(this.skorOyuncu2);
+            this.Controls.Add(this.skorOyuncu1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Movable = false;
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.Text = "Zar Oyunu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label skorOyuncu1;
+        private System.Windows.Forms.Label skorOyuncu2;
+        private MetroFramework.Controls.MetroButton btnOyuncu1;
+        private MetroFramework.Controls.MetroButton btnOyuncu2;
+    }
+}
+
---
+++ 2OyunculuZarOyunu/2OyunculuZarOyunu/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _2OyunculuZarOyunu
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        int Oyuncu1zarSonDeger=0;
+        int Oyuncu2zarSonDeger=0;
+        int ResimGosterilmeSayisi = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ZarDegistir(int i)
+        {
+            Application.DoEvents();
+            System.Threading.Thread.Sleep(200);
+            switch (i)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.zar1;
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.zar2;
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.zar3;
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources.zar4;
+                    break;
+                case 5:
+                    pictureBox1.Image = Properties.Resources.zar5;
+                    break;
+                case 6:
+                    pictureBox1.Image = Properties.Resources.zar6;
+                    break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            if (rnd.Next(0, 2) == 0)
+            {
+                btnOyuncu1.Visible = true;
+                btnOyuncu2.Visible = false;
+            }
+            else
+            {
+                btnOyuncu1.Visible = false;
+                btnOyuncu2.Visible = true;
+            }
+        }
+        
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                Oyuncu1zarSonDeger = rnd.Next(0, 7);
+                ZarDegistir(Oyuncu1zarSonDeger);
+            }
+            btnOyuncu1.Visible = false;
+            btnOyuncu2.Visible = true;
+            ResimGosterilmeSayisi++;
+            if (ResimGosterilmeSayisi % 2 == 0)
+            {
+                SkorKontrol();
+            }
+        }
+
+        private void SkorKontrol()
+        {
+            int skor;
+            if (Oyuncu1zarSonDeger > Oyuncu2zarSonDeger)
+            {
+                skor = Convert.ToInt32(skorOyuncu1.Text);
+                skor++;
+                skorOyuncu1.Text = skor.ToString();
+            }
+            else
+            {
+                skor = Convert.ToInt32(skorOyuncu2.Text);
+                skor++;
+                skorOyuncu2.Text = skor.ToString();
+            }
+            Oyuncu1zarSonDeger = 0;
+            Oyuncu2zarSonDeger = 0;
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                Oyuncu2zarSonDeger = rnd.Next(0, 7);
+                ZarDegistir(Oyuncu2zarSonDeger);
+            }
+            btnOyuncu1.Visible = true;
+            btnOyuncu2.Visible = false;
+            ResimGosterilmeSayisi++;
+            if (ResimGosterilmeSayisi % 2 == 0)
+            {
+                SkorKontrol();
+            }
+        }
+    }
+}
